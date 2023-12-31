f6da7466ba6bd7dfaf2dfb0dd1a8f3e8fcd2abf9
Matheus Gonçalves <mgdesenvolvedor@gmail.com>
2023-12-30 21:38:59 -0300

    style:changing name next event

--- src/components/NextEvent/NextEvent.tsx
+++ src/components/NextEvent/NextEvent.tsx
@@ -1,7 +1,7 @@
 export default function NextEvent() {
     return (
         <section className='w-full h-10 bg-page-color text-center text-2xl text-seal-brown antialiased'>
-            <h2>Próximo Evento: </h2>
+            <h2>Próximo Evento: Imperial Geek Fest - 13/01/2024</h2>
         </section>
     );
 }
