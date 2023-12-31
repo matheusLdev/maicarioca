709d8943cc21fcad72e3f520b22a3ab55eb8e57a
Matheus Gonçalves <mgdesenvolvedor@gmail.com>
2023-12-30 21:28:44 -0300

    feat: add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className='w-full h-20 bg-header-footer flex justify-around items-center text-slate-100 antialiased text-base'>
+            <p>Todos os direitos reservados - @maicarioca</p>
+            <p>Desenvolvido por Matheus Gonçalves</p>
+        </footer>
+    );
+}
