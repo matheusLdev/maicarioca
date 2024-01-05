7d64301c2ee2a48ee4bea2b6fe5413bd51106cbb
Matheus Gonçalves <mgdesenvolvedor@gmail.com>
2024-01-05 18:50:53 -0300

    style: reducing footer text size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className='h-20 bg-header-footer flex flex-wrap justify-around items-center text-slate-100 text-base'>
+        <footer className='h-20 bg-header-footer flex flex-wrap justify-around items-center text-slate-100 text-xs md:text-base'>
             <p>Todos os direitos reservados - @maicarioca</p>
             <p>Desenvolvido por Matheus Gonçalves</p>
         </footer>
